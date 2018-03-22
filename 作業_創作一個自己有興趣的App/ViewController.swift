d69d7fd015d1ba1bc553f42b33283ebfa75ccfef
Jony Wu <jonywu@2001-b011-1002-1113-0834-9b1a-3f4d-d7a3.dynamic-ip6.hinet.net>
2018-03-22 15:49:21 +0800

    Initial Commit

---
+++ 作業_創作一個自己有興趣的App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  作業_創作一個自己有興趣的App
+//
+//  Created by Jony Wu on 2018/3/22.
+//  Copyright © 2018年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
