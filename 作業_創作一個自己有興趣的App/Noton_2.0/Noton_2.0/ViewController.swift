6940c2d0f46df95f4bfd6fdb25ea8cdbb0eaa82d
Jony Wu <jonywu@jonyteki-macbook-pro.local>
2018-04-04 00:38:12 +0800

    NOTON APP 2.0

---
+++ 作業_創作一個自己有興趣的App/Noton_2.0/Noton_2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Noton_2.0
+//
+//  Created by Jony Wu on 2018/3/28.
+//  Copyright © 2018年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
